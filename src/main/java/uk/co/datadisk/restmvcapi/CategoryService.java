aab2e08457d0407205dc4a82fe258a70fa43207f
pvalle <paul.valle@datadisk.co.uk>
2018-02-16 08:48:14 +0000

    got the first api working

---
+++ src/main/java/uk/co/datadisk/restmvcapi/CategoryService.java
@@ -0,0 +1,11 @@
+package uk.co.datadisk.restmvcapi;
+
+import uk.co.datadisk.restmvcapi.api.v1.model.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
+}
--- src/main/java/uk/co/datadisk/restmvcapi/repositories/CategoryRepository.java
+++ src/main/java/uk/co/datadisk/restmvcapi/repositories/CategoryRepository.java
@@ -7,4 +7,6 @@ import uk.co.datadisk.restmvcapi.domain.Category;
  * Created by jt on 9/24/17.
  */
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Category findByName(String name);
 }
